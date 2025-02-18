eccee30fa868de9a6373ea64bcc7e253167cf01c
brian <brianarguello.vga>
2025-02-18 12:31:17 -0300

    configurando nombre de salida

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from PIL import Image
-import os
 import sys
-
+from datetime import datetime
 print("Conversor De Archivos".center(50, "*"))
 
 if len(sys.argv) != 2:
@@ -9,4 +8,31 @@ if len(sys.argv) != 2:
     exit()
 
 parametro = sys.argv[1]
-print(parametro)
+imagen = Image.open(parametro)
+
+print("""Ingresa a que formato quieres convertir esta imagen:
+      1.- JPEG
+      2.- PNG
+      3.- BMP
+      4.- GIF
+      5.- TIFF
+      6.- WEBP""")
+
+opcion = input("Elige un formato: ")
+
+formato = {
+    "1": "JPEG",
+    "2": "PNG",
+    "3": "BMP",
+    "4": "GIF",
+    "5": "TIFF",
+    "6": "WEBP"
+}
+
+if opcion not in formato:
+    print("Error: opcion no valida")
+    exit()
+
+formato_selecionado = formato[opcion].lower()
+nuevo_nombre = f"{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}.{formato_selecionado}"
+print(nuevo_nombre)
