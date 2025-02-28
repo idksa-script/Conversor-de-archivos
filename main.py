a61cc2d261bccf1990649930d21aaefa191e3149
idksa-script <brianarguello358@gmail.com>
2025-02-27 22:39:35 -0300

    funcion pdf a imagenes

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from PIL import Image
 import sys
+import os
 from datetime import datetime
+from pdf2image import convert_from_path
+
 print("Conversor De Archivos".center(50,"*"))
 
 if len(sys.argv) !=2:
@@ -10,10 +13,21 @@ if len(sys.argv) !=2:
 parametro = sys.argv[1]
 
 def imagen_a_otro_formato(opcion, formato, imagen):
+
     formato_selecionado = formato[opcion].lower()
     nuevo_nombre = f"{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}.{formato_selecionado}"
     imagen.save(nuevo_nombre, formato_selecionado)
     return nuevo_nombre
+
+def pdf_a_imagenes(parametro, formato="png"):
+
+    carpeta_salida = os.getcwd()
+
+    imagenes = convert_from_path(parametro, fmt=formato)
+    for i, imagen in enumerate(imagenes):
+        nombre_imagen = f"pagina_(i + 1).{formato}"
+        imagen.save(nombre_imagen, formato.upper())
+    return print("correcto")
 
 print("""Ingresa a que tipo de archivo quieres convertir:
         1.- Una imagen a otro formato 2.- PDF a imagenes""")
@@ -42,3 +56,7 @@ if eleccion == 1:
     imagen_a_otro_formato(opcion, formato, imagen)
     print("Fin")
 
+elif eleccion == 2:
+
+    pdf_a_imagenes(parametro, formato="png")
+    
